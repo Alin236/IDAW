ceb490bfd1269810e5f853edddbb9b6012b51efe
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-03-08 16:42:33 +0100

    Ajustement d'espace

--- tp2/exo6/accueil.php
+++ tp2/exo6/accueil.php
@@ -1,5 +1,5 @@
 <div class="contenu">
-            <h2>Bienvenue</h2>
-            <p>Ce site est le TP2 de l'UV IDAW (Introduction au Développement d'Application Web).</p>
-            <p>Ce TP a pour but de faire découvrir le langage de programmation <em>php</em>.</p>
-        </div>
+    <h2>Bienvenue</h2>
+    <p>Ce site est le TP2 de l'UV IDAW (Introduction au Développement d'Application Web).</p>
+    <p>Ce TP a pour but de faire découvrir le langage de programmation <em>php</em>.</p>
+</div>
