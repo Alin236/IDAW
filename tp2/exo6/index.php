174a44d06d0f131704c1b1894a9e08e66ce0418d
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-03-08 16:28:42 +0100

    Extraction du corps du code d'index.php dans accueil.php

--- tp2/exo6/index.php
+++ tp2/exo6/index.php
@@ -4,10 +4,5 @@
             require_once("template_menu.php");
             renderMenuToHTML("index");
         ?>
-        <div class="contenu">
-            <h2>Bienvenue</h2>
-            <p>Ce site est le TP2 de l'UV IDAW (Introduction au Développement d'Application Web).</p>
-            <p>Ce TP a pour but de faire découvrir le langage de programmation <em>php</em>.</p>
-        </div>
     </div>
 <?php require_once("template_footer.php")?>
