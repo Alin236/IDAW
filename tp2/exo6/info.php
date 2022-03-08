ef000ffca23878877ecc952621c8a35d714311dd
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-03-08 16:26:45 +0100

    Copie de l'exo3 pour l'exo6

---
+++ tp2/exo6/info.php
@@ -0,0 +1,15 @@
+<?php require_once("template_header.php")?>
+    <div class="container">
+        <?php
+            require_once("template_menu.php");
+            renderMenuToHTML("info");
+        ?>
+        <div class="contenu">
+            <h2>Info du site</h2>
+            <h3>Auteur :</h3>
+            <p>Nilavan DEVA</p>
+            <h3>Dernière modification :</h3>
+            <p>Le 8 mars 2022.</p>
+        </div>
+    </div>
+<?php require_once("template_footer.php")?>
---
+++ tp2/exo6/template_header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+<head> 
+    <title>Mon site</title> 
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link href="tp1.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <header><h1>Mon site</h1></header>
