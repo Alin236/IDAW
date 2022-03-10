e93b5627700138f71e7966f31aeab6837137189f
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-03-11 00:15:12 +0100

    Ajout de contenu contact

--- tp2/exo8/fr/contact.php
+++ tp2/exo8/fr/contact.php
@@ -1,3 +1,5 @@
 <h2>Me contacter</h2>
-<p>Pas d'information de contact.</p>
-<p>Pour ajouter cette page, et qu'elle ait a propre entrée dans le menu, il suffit de modifier la variable $mymenu dans template_menu.php en rajoutant la clé et valeur "contact" et "Contact".</p>
+<h3>LinkedIn</h3>
+<p><a href="https://www.linkedin.com/in/nilavan-deva-9629ab211">Nilavan DEVA</a></p>
+<h3>Email</h3>
+<p>nilavan.deva@etu.imt-nord-europe.fr</p>
