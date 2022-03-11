ffd6953fbd67e0ade0840b9d0e908ff09a015114
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-03-11 07:24:19 +0100

    Correction

--- tp2/exo8/fr/projets.php
+++ tp2/exo8/fr/projets.php
@@ -3,7 +3,7 @@
 <p>Une application Flutter permettant de jouer au Taquin avec des images contenu dans le téléphone, prise en directe ou encore reçu par un site donnant des images aléatoires.</p>
 <img src="images/Taquin.jpg" alt="Taquin">
 <h3>Hammer</h3>
-<p>Un jeu iOS dans lequel le joueur doit secouer le plus fortement possible son iPhone</p>
+<p>Un jeu iOS dans lequel le joueur doit secouer le plus fortement possible son iPhone.</p>
 <h3>AsteroidMania</h3>
 <p>Un jeu Android dans lequel le joueur doit esquiver des astéroïde.</p>
 <img src="images/AsteroidMania.jpg" alt="AsteroidMania">
