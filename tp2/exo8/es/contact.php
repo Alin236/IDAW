303f6256aada470e91e3de23ee8246138c530b89
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-03-11 09:25:21 +0100

    Traduction espagnol

---
+++ tp2/exo8/es/contact.php
@@ -0,0 +1,5 @@
+<h2>Contactarme</h2>
+<h3>LinkedIn</h3>
+<p><a href="https://www.linkedin.com/in/nilavan-deva-9629ab211">Nilavan DEVA</a></p>
+<h3>Email</h3>
+<p>nilavan.deva@etu.imt-nord-europe.fr</p>
