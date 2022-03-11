1b5cbfe6755eb0e4d29b74a1986fafabfcdfd872
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-03-11 10:17:22 +0100

    Altération du mail

--- tp2/exo8/es/contact.php
+++ tp2/exo8/es/contact.php
@@ -2,4 +2,4 @@
 <h3>LinkedIn</h3>
 <p><a href="https://www.linkedin.com/in/nilavan-deva-9629ab211">Nilavan DEVA</a></p>
 <h3>Email</h3>
-<p>nilavan.deva@etu.imt-nord-europe.fr</p>
+<p>nilavan.deva**at**etu.imt-nord-europe.fr</p>
