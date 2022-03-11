555b050e2e3da3b06e167fc1487e90e3f53d2ad0
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-03-11 09:25:47 +0100

    Correction error

--- tp2/exo8/en/error.php
+++ tp2/exo8/en/error.php
@@ -1,2 +1,2 @@
-<h2>Erreur 404</h2>
-<p>The page that you are seeking does not exist or is not translated in english</p>
+<h2>Error 404</h2>
+<p>The page that you are seeking does not exist or is not translated in english.</p>
