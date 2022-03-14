85e12e5e475f8f07d439791ed5ff526460d38510
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-03-14 10:00:23 +0100

    Effectue une requête et affiche le résultat

--- tp3/base_de_donnee/testConnexion.php
+++ tp3/base_de_donnee/testConnexion.php
@@ -2,13 +2,36 @@
     $servername = 'localhost';
     $username = 'root';
     $password = '';
+    $database = 'idaw-php-sql';
     
     //On établit la connexion
-    $connection = new mysqli($servername, $username, $password);
+    $connection = new mysqli($servername, $username, $password, $database);
     
     //On vérifie la connexion
     if($connection->connect_error){
         die('Erreur : ' .$connection->connect_error);
     }
-    echo 'yeahye';
+    $result = $connection->query('SELECT * FROM User');
+    if($result != false){
+        $all = $result->fetch_all();
+        echo '<table>
+                <tr>
+                    <th>id</th>
+                    <th>login</th>
+                    <th>password</th>
+                    <th>pseudo</th>
+                </tr>';
+        foreach($all as $row){
+            echo "<tr>
+                    <td>$row[0]</td>
+                    <td>$row[1]</td>
+                    <td>$row[2]</td>
+                    <td>$row[3]</td>
+                </tr>";
+        }
+        echo '</table>';
+    }
+    else{
+        echo '<p>échec de la query</p>';
+    }
 ?>
