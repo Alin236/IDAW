11ac22e7c9ce1fbdc51261e055418a2d512761ea
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-03-14 08:55:50 +0100

    Un lien pour se déconnecter a été ajouté

--- tp3/sessions/login.php
+++ tp3/sessions/login.php
@@ -1,17 +1,33 @@
-<form id="login_form" action="connected.php" method="POST">
-    <table>
-        <tr>
-            <th>Login :</th>
-            <td><input type="text" name="login"></td>
-        </tr>
-        <tr>
-            <th>Mot de passe :</th>
-            <td><input type="password" name="password"></td>
-        </tr>
-        <tr>
-            <th></th>
-            <td><input type="submit" value="Se connecter..." /></td>
-        </tr>
-    </table>
-</form>
+<?php
+    $formulaire =
+        '<form id="login_form" action="connected.php" method="POST">
+            <table>
+                <tr>
+                    <th>Login :</th>
+                    <td><input type="text" name="login"></td>
+                </tr>
+                <tr>
+                    <th>Mot de passe :</th>
+                    <td><input type="password" name="password"></td>
+                </tr>
+                <tr>
+                    <th></th>
+                    <td><input type="submit" value="Se connecter..." /></td>
+                </tr>
+            </table>
+        </form>';
+    session_start();
+    if(!isset($_SESSION['login'])){
+        echo $formulaire;
+    } else if(isset($_GET['disconnect']) && $_GET['disconnect'] == 'true'){
+        echo $formulaire;
+        unset($_SESSION['login']);
+        echo '<p>vous avez été déconnecté</p>';
+    }
+    else {
+        $login = $_SESSION['login'];
+        echo "<p>Vous êtes déjà connecté sous le nom $login";
+        echo '<p><a href="?disconnect=true">Click ici pour se déconnecter</a></p>';
+    }
+?>
 <p><a href="connected.php">login.php</a></p>
