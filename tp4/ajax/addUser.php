39029e28a62df69b857ecaac747d2266ebc44394
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-03-15 12:01:54 +0100

    fin de cours, actuellement sur add un utilisateur

--- tp4/ajax/addUser.php
+++ tp4/ajax/addUser.php
@@ -10,19 +10,23 @@
 
     //On vérifie la connexion
     if($connection->connect_error){
+        echo false;
         die('Erreur : ' .$connection->connect_error);
     }
 
-    if(isset($_POST['nom']) && isset($_POST['prenom'])){
+    if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['dateNaissance']) && isset($_POST['aimeCours']) && isset($_POST['commentaire'])){
         $nom = $connection->real_escape_string($_POST['nom']);
         $prenom = $connection->real_escape_string($_POST['prenom']);
+        $dateNaissance = $connection->real_escape_string($_POST['dateNaissance']);
+        $aimeCours = $connection->real_escape_string($_POST['aimeCours']);
+        $commentaire = $connection->real_escape_string($_POST['commentaire']);
 
-        $query = "INSERT INTO utilisateur (id, nom, prenom) VALUES (NULL, '$nom', '$prenom')";
+        $query = "INSERT INTO utilisateur (id, nom, prenom, date_naissance, aime_cours, commentaire) VALUES (NULL, '$nom', '$prenom', '$dateNaissance', $aimeCours, '$commentaire')";
         $result = $connection->query($query);
 
-        echo $result;
+        echo '{"a":2}';
     }
     else{
-        echo 'fuyvhv';
+        echo 'avdvkdnv';
     }
 ?>
